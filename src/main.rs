eb4e1b7f2670f13e06ed07913e66814644f3e5b3
lverno <lverno321@gmail.com>
2022-08-30 21:42:02 -0400

    simplified code

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,67 @@
 // Prevent terminal window from appearing (in release mode)
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod data;
-
-use data::ResponseData;
 use notify_rust::Notification;
 use reqwest::blocking::{multipart::Form, Client};
-use std::time::Duration;
+use serde_json::Value;
+use std::{path::Path, time::Duration};
 
 /// Upload URL
 const URL: &str = "https://api.bayfiles.com/upload";
 
 fn main() {
     if let Some(path) = std::env::args().nth(1) {
-        // Create HTTP client and send POST request
-        let resp: ResponseData = Client::new()
+        // Extract filename from path
+        let filename = Path::new(&path)
+            .file_name()
+            .map(|s| s.to_str())
+            .flatten()
+            .unwrap_or("file");
+
+        // Send request with file
+        let resp: Value = Client::new()
             .post(URL)
-            .multipart(
-                // Create form data with filepath
-                Form::new()
-                    .file("file", path)
-                    .unwrap_or_notify("Error: unable to read file"),
-            )
+            .multipart(Form::new().file("file", &path).unwrap_or_else(|_| {
+                notify_exit(
+                    &format!("Error uploading {filename}"),
+                    "failed to read file",
+                    1,
+                )
+            }))
             // Disable timeout
             .timeout(Duration::from_secs(999999))
             .send()
-            .unwrap_or_notify("Error: failed to upload file. Check your internet connection.")
-            // Deserialize the JSON data into ResponseData
+            .unwrap_or_else(|_| {
+                notify_exit(
+                    &format!("Error uploading {filename}"),
+                    "failed to upload file",
+                    1,
+                )
+            })
+            // Deserialize JSON
             .json()
-            .unwrap_or_notify("Error: received invalid response");
+            .unwrap_or_else(|_| {
+                notify_exit(
+                    &format!("Error uploading {filename}"),
+                    "received invalid response",
+                    1,
+                )
+            });
 
-        // Check response status
-        if resp.status {
-            // Get the URL from the response data
-            let url = resp.url();
-            // Copy URL to clipboard and send notification
-            if clipboard_win::set_clipboard_string(url).is_ok() {
-                notify2("Link copied to clipboard", url);
-            } else {
-                notify2(url, "Could not copy link to clipboard");
+        if resp["status"].as_bool().unwrap_or(false) {
+            if let Some(link) = resp["data"]["file"]["url"]["short"].as_str() {
+                if clipboard_win::set_clipboard_string(link).is_ok() {
+                    notify_exit("Link copied to clipboard", link, 0)
+                } else {
+                    notify_exit("Failed to copy link to clipboard", link, 1)
+                }
             }
-        } else {
-            // Send an error notification with the error message provided by the response.
-            notify(&format!("Error: {}", resp.err_msg()));
         }
     }
 }
 
-/// Send a toast notification with a message.
-fn notify(text: &str) {
-    drop(Notification::new().body(text).show())
-}
-
-/// Send a toast notification with a title and a message.
-fn notify2(title: &str, text: &str) {
-    drop(Notification::new().summary(title).body(text).show())
-}
-
-/// Custom error handling extension method.
-trait ResultExt<T> {
-    fn unwrap_or_notify(self, msg: &str) -> T;
-}
-impl<T, E: std::error::Error> ResultExt<T> for Result<T, E> {
-    fn unwrap_or_notify(self, msg: &str) -> T {
-        self.unwrap_or_else(|e| {
-            // Send an error notification with the message and then panic with the error object
-            notify(msg);
-            panic!("{}", e);
-        })
-    }
+/// Send a toast notification with a title and a message, then exit with an exit code.
+fn notify_exit(title: &str, text: &str, code: i32) -> ! {
+    drop(Notification::new().summary(title).body(text).show());
+    std::process::exit(code);
 }
